0959c28dd5e67f26b6216d0914c156c0a643b381
developerTajul <developer.tajul@gmail.com>
2018-04-17 10:06:23 +0600

    comment section added

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+/*
+Plugin Name:  Spellbit Quiz
+Plugin URI:   http://spellbit.com
+Description:  This is the description from here
+Version:      1.0.1
+Author:       Spellbit Team
+Author URI:   http://spellbit.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  spellbit
+Domain Path:  /languages
+*/
+
+
